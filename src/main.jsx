e4ae28703317ba34adb18332f9ddeaf65345b39c
Gilbert Haro <gilbertharocode@gmail.com>
2024-11-25 22:17:37 -0800

    Redux file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import store from './store.js';
+
+store.dispatch({ type: 'account: desposit', payload: 250 });
+console.log(store.getState());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
