8bc27b9c6928c00f41dd0316184a62042744c8e4
Gilbert Haro <gilbertharocode@gmail.com>
2024-11-25 23:51:50 -0800

    compose with devTools

--- src/store.jsx
+++ src/store.jsx
@@ -1,13 +1,14 @@
 import { combineReducers, createStore } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
 
